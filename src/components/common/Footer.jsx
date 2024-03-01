6ea3767ad81dc9862f97e84a1355f5562dce5d48
designs-by-kate <kate_diet@hotmail.com>
2024-03-01 13:35:45 +0000

    Implemented icons with links to GitHub, LinkedIn and Instagram, as well as based styling

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,12 +1,26 @@
 // components/common/Footer.jsx
 import React from 'react';
+import './Footer.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Footer = () => {
   return (
-    <footer>
-      {/* Your footer content goes here */}
-      <p>&copy; 2024 My React App</p>
-    </footer>
+    <footer className="footer">
+    <div className="footer-container">
+      <div className="social-icons">
+        <a href="https://github.com/designs-by-kate" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/kate-pospiech-3b8846290/" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="https://www.instagram.com/hadzinskak/" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
+      <p>&copy; 2024 Kate's Portfolio</p>
+    </div>
+  </footer>
   );
 };
 
