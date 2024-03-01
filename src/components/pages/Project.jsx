24db1bd15914b498516d3d4999bcab21c2789f7c
designs-by-kate <kate_diet@hotmail.com>
2024-03-01 23:42:50 +0000

    Implemented structure and styling for Project Page

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,12 +1,38 @@
 // pages/Project.jsx
-import React from 'react';
-import '../../assets/styles/Project.css'; 
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../assets/styles/Project.css';
+import project from '../../data/project.json';
 
-const Project = () => {
+function Project() {
+
+  const [projectData, setProjectData] = useState(project);
+
   return (
-    <div>
-      {/* Individual Project page content goes here */}
-      <h2>Project Details</h2>
+    <div className="container project-gallery">
+      <h2 className="text-center">Project Details</h2>
+
+      <div className="row">
+        {projectData.map(project => (
+          <div key={project.id} className="col-12 mb-4">
+            <h3 className="project-title text-center">{project.title}</h3>
+            <div className="row">
+              <div className="col-md-2">
+                <img src={project.image} alt={project.title} className="project-image img-fluid" />
+              </div>
+              <div className="project-details col-md-10">
+                <p><strong>Description:</strong> {project.description}</p>
+                <p><strong>Project Type:</strong> {project.projectType}</p>
+                <p><strong>Role:</strong> {project.role}</p>
+                <p><strong>Dependencies:</strong> {project.dependencies}</p>
+                <p><strong>Tools Used:</strong> {project.tools}</p>
+                <p><strong>Deployed URL:</strong> <a href={project.deployedURL} target="_blank" rel="noopener noreferrer">{project.deployedURL}</a></p>
+                <p><strong>GitHub URL:</strong> <a href={project.gitHubUrl} target="_blank" rel="noopener noreferrer">{project.gitHubUrl}</a></p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
